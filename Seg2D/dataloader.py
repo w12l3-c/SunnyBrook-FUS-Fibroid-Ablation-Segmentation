820e10d5fcedc55dc26047718934d6133d63903d
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-15 15:58:36 -0400

    Standard Equalize histogram to make it unbiased of gamma correction

--- Seg2D/dataloader.py
+++ Seg2D/dataloader.py
@@ -69,14 +69,14 @@ def convert_to_PIL(pairs, img_size=(320, 320)):
         #     pixel_spaces.append(pixel_spaceing)
         
         img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-        # img = cv2.equalizeHist(img.astype(np.uint8))
+        img = cv2.equalizeHist(img.astype(np.uint8))
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         
         img = Image.fromarray(img)
         img = img.resize(img_size)
         img = img.convert("RGB")
         # img = ImageOps.equalize(img)
-        img = gamma_correction_pil(img, gamma=1.5)
+        # img = gamma_correction_pil(img, gamma=1.5)
         
         mask = Image.open(mask)
         mask = mask.resize(img_size)
