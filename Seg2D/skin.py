0d331f461f86b038f36a7decdf2614181c710c99
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-17 16:22:05 -0400

    sf

--- Seg2D/skin.py
+++ Seg2D/skin.py
@@ -30,7 +30,7 @@ from train import deeplabv3_train_model, unet_train_model
 
 from models import DeepLabV3, Unet, Unetpp
 from model_inference_fn import deeplabv3_inference, unet_inference
-from model_run_fn import deeplabv3_run, unet_run, unetpp_run
+from model_run_fn import deeplabv3_run, unet_run, unetpp_run, deeplabv3p_run
 
 # This will be just segmenting skin (1 class) -> Binary Segmentation Task
 
@@ -40,7 +40,7 @@ PIN_MEMORY = True   # Pin memory for faster GPU transfer
 NUM_EPOCHS = 300 # Just fot test, in pratical should be 100 or more
 BATCH_SIZE = 8  # Between 8-16 is good
 IN_CHANNELS = 3 # RGB
-NUM_CLASSES = 1 # Classes to Segment
+NUM_CLASSES = 2 # Classes to Segment background and foreground
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # =================== Patient Dataset =================== #
@@ -70,23 +70,27 @@ test_skin_dataset = convert_to_PIL(test_skin_dataset)
 
 if __name__ == "__main__":
     try:
-        unet_save_path = f"/mnt/HDD_1TB/Wallace/Code/Seg2D/trained_models/Unet_Skin_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.pth"
-        unet_writer_path = f"runs/Unet_Skin_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}"
-        unet_run(train_skin_dataset, val_skin_dataset, unet_save_path, unet_writer_path, device, display=False)
+        unet_save_path = f"/mnt/HDD_1TB/Wallace/Code/Seg2D/trained_models/Unet_Skin_{datetime.datetime.now().strftime('%Y-%m-%d_%H')}.pth"
+        unet_writer_path = f"runs/Unet_Skin_{datetime.datetime.now().strftime('%Y-%m-%d_%H')}"
+        unet_run(train_skin_dataset, val_skin_dataset, device, NUM_EPOCHS, NUM_CLASSES, BATCH_SIZE, NUM_WORKERS, True, unet_save_path, unet_writer_path, 'BCE')
     except Exception as e:
         print(e)
         print('Session Crashed')
     
     try:
-        unet_path = "/mnt/HDD_1TB/Wallace/Code/Seg2D/trained_models/Unet_Skin_2023-08-03_14:03:42.pth"
+        unet_path = "/mnt/HDD_1TB/Wallace/Code/Seg2D/trained_models/Unet_Skin_2023-08-03_14.pth"
         unet_inference(test_skin_dataset, unet_path, device, display=False)
     except Exception as e:
         print(e)
         print('Session Crashed')
-
-    
- 
-    
+        
+    try:
+        deeplabv3p_writer = f"runs/DeepLabV3P_Skin_{datetime.datetime.now().strftime('%Y-%m-%d_%H')}"
+        deeplabv3p_save_path = f"/mnt/HDD_1TB/Wallace/Code/Seg2D/trained_models/DeepLabV3P_Skin_{datetime.datetime.now().strftime('%Y-%m-%d_%H')}.pth"
+        deeplabv3p_run(train_skin_dataset, val_skin_dataset, device, NUM_EPOCHS, NUM_CLASSES, BATCH_SIZE, NUM_WORKERS, True, deeplabv3p_writer, deeplabv3p_save_path, 'BCE')
+    except Exception as e:
+        print(e)
+        print('This training sessions failed')
     
     
     
