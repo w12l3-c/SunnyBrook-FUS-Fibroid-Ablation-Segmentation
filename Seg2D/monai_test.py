c8f9bfa2687bdf2e3709bf0abbf6e0900a8f5281
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-18 15:53:29 -0400

    Monai

--- Seg2D/monai_test.py
+++ Seg2D/monai_test.py
@@ -1,3 +1,78 @@
 from monai.utils import first, set_determinism
+from monai.transforms import (
+    AsDiscrete,
+    AsDiscreted,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandAffined,
+    RandFlipd,
+    RandScaleIntensityd,
+    RandShiftIntensityd,
+    RandSpatialCropd,
+    SaveImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+    Invertd,
+)
+from monai.data import CacheDataset, DataLoader, Dataset, decollate_batch
 
 set_determinism(seed=0)
+
+train_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys=["image", "label"]),
+        ScaleIntensityRanged(
+            keys=["image"], a_min=-1000, a_max=1000,
+            b_min=0.0, b_max=1.0, clip=True,
+        ),
+        
+        # Change labels from 255 to 1
+        ScaleIntensityRanged(
+            keys=["label"], a_min=0, a_max=255,
+            b_min=0.0, b_max=1.0, clip=True,
+        ),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        Orientationd(keys=["image", "label"], axcodes="PLS"),
+        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+        
+        # Randomly crop a patch from both image and label files
+        RandSpatialCropd(
+                keys=["image", "label"],
+                roi_size=[96, 96, 96],
+                random_size=False,
+        ),
+     
+        # Data augmentation transforms
+        RandAffined(
+            keys=['image', 'label'],
+            mode=('bilinear', 'nearest'),
+            prob=1.0, spatial_size=(96, 96, 96),
+            rotate_range=(0, 0, np.pi/15),
+            scale_range=(0.1, 0.1, 0.1)),
+        RandFlipd(keys=["image", "label"], prob=0.25, spatial_axis=0),
+        RandFlipd(keys=["image", "label"], prob=0.25, spatial_axis=1),
+        RandFlipd(keys=["image", "label"], prob=0.25, spatial_axis=2),
+        RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
+        RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
+        ])
+
+val_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys=["image", "label"]),
+        ScaleIntensityRanged(
+            keys=["image"], a_min=-1000, a_max=1000,
+            b_min=0.0, b_max=1.0, clip=True,
+        ),
+        ScaleIntensityRanged(
+            keys=["label"], a_min=0, a_max=255,
+            b_min=0.0, b_max=1.0, clip=True,
+        ),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        Orientationd(keys=["image", "label"], axcodes="PLS"),
+        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+    ])
