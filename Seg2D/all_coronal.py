9405c00b36708bb54482bb7af893fdeee2929e2f
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-18 10:08:51 -0400

    a

--- Seg2D/all_coronal.py
+++ Seg2D/all_coronal.py
@@ -69,6 +69,8 @@ train_sag_dataset = convert_to_PIL_multi(train_sag_dataset, COLOR_DICT)
 val_sag_dataset = convert_to_PIL_multi(val_sag_dataset, COLOR_DICT)
 test_sag_dataset = convert_to_PIL_multi(test_sag_dataset, COLOR_DICT)
 
+
+
 dataset_dict = Beit3.create_huggingface_dataset(train_sag_dataset, val_sag_dataset, test_sag_dataset)
 
 
