14b100c73df8c6415f04a4a39f743f9040ecd154
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-18 10:13:45 -0400

    a

--- Seg2D/all_coronal.py
+++ Seg2D/all_coronal.py
@@ -43,6 +43,8 @@ BATCH_SIZE = 4  # Between 8-16 is good
 IN_CHANNELS = 3 # RGB
 COLOR_DICT = {'Spine': (35, 132, 250), 'Bowel': (14, 240, 56), 'Muscle': (214, 51, 36), 'Skin': (240, 170, 31), 'hip_L': (173, 20, 250), 'hip_R': (131, 20, 250)}   # Color map in dictionart
 COLOR_LIST = [v for v in COLOR_DICT.values()]   # Colour map in list
+ID2LABEL = {int(k): v for k, v in COLOR_DICT.items()}
+LABEL2ID = {v: k for k, v in COLOR_DICT.items()}
 NUM_CLASSES = len(COLOR_LIST) # Classes to Segment
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -60,18 +62,18 @@ test_patients = create_patient_list(path_list=test_path_list)
 
 # =================== Coronal Datasets =================== #
 # Grab Each Patient's Coronal image and mask list
-train_sag_dataset = get_multilabel_coronal(train_patients)
-test_sag_dataset = get_multilabel_coronal(test_patients)
-train_sag_dataset, val_sag_dataset = train_test_split(train_sag_dataset, test_size=0.1, random_state=42)
+train_cor_dataset = get_multilabel_coronal(train_patients)
+test_cor_dataset = get_multilabel_coronal(test_patients)
+train_cor_dataset, val_cor_dataset = train_test_split(train_cor_dataset, test_size=0.1, random_state=42)
 
 # Convert the pydicom and mask list to PIL images
-train_sag_dataset = convert_to_PIL_multi(train_sag_dataset, COLOR_DICT)
-val_sag_dataset = convert_to_PIL_multi(val_sag_dataset, COLOR_DICT)
-test_sag_dataset = convert_to_PIL_multi(test_sag_dataset, COLOR_DICT)
+train_cor_dataset = convert_to_PIL_multi(train_cor_dataset, COLOR_DICT)
+val_cor_dataset = convert_to_PIL_multi(val_cor_dataset, COLOR_DICT)
+test_cor_dataset = convert_to_PIL_multi(test_cor_dataset, COLOR_DICT)
 
 
 
-dataset_dict = Beit3.create_huggingface_dataset(train_sag_dataset, val_sag_dataset, test_sag_dataset)
+dataset_dict = Beit3.create_huggingface_dataset(train_cor_dataset, val_cor_dataset, test_cor_dataset)
 
 
 # if __name__ == '__main__':
