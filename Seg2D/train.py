18129aef29a2130a90f18b539f834c67a7b7225b
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-21 09:45:45 -0400

    Comment

--- Seg2D/train.py
+++ Seg2D/train.py
@@ -280,8 +280,11 @@ def unet_train_step(model, dataloader, loss_fn, accuracy, optimizer, scheduler, 
         if batch % int(len(dataloader)*0.2) == 0 and batch != 0:
             print(f"Progress: {batch}/{len(dataloader)} | Train loss: {train_loss:.4f} | Train acc: {train_acc:.4f}")
 
+    # Compute loss and accuracy for the epoch
     train_loss /= len(dataloader)
     train_acc /= len(dataloader)
+    
+    # Update learning rate with scheduler
     scheduler.step(train_loss)
 
     return train_loss, train_acc
@@ -303,21 +306,27 @@ def unet_val_step(model, dataloader, loss_fn, accuracy, weight_fn, device):
     Returns:
         Tuple[float, float]: Validation loss and accuracy for the current step.
     """
-    model.eval()
+    model.eval()    # Evaluation mode - Gradients are not changable
     val_loss = 0
     val_acc = 0
 
-    with torch.inference_mode():
+    # Inference
+    with torch.inference_mode():    
+        # Loop over each batch  
         for batch, (img, mask) in enumerate(dataloader):
+            # Push tensor to gpu or cpu
             img = img.to(device)
             mask = mask.to(device)
-            mask = mask.squeeze()
+            mask = mask.squeeze()   # Change mask shape for accuarcy and loss function
 
+            # Compute class weight for loss function
             pos_weight = weight_fn(mask).to(device)
 
+            # Forward pass
             y_logits = model(img)
             y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1).float()
 
+            # Compute loss and accuracy
             acc = accuracy(y_pred, mask)
             loss = loss_fn(y_logits[:, 1], mask) * pos_weight
 
@@ -327,6 +336,7 @@ def unet_val_step(model, dataloader, loss_fn, accuracy, weight_fn, device):
             if batch % 4 == 0 and batch != 0:
                 print(f"Progress: {batch}/{len(dataloader)} | Val loss: {val_loss:.4f} | Val acc: {val_acc:.4f}")
 
+        # Compute loss and accuracy for the epoch
         val_loss /= len(dataloader)
         val_acc /= len(dataloader)
 
@@ -355,14 +365,19 @@ def unet_train_model(model, train_dataloader, val_dataloader, loss_fn, accuracy,
     Returns:
         Tuple[dict, dict]: Best model state dict and training results (loss and accuracy).
     """
+    # Initialize results in a dictionary
     results = { "train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
     
+    # Initialize early stopper
     early_stopper = EarlyStopper(patience=5, min_delta=5)
 
+    # Training
     for epoch in tqdm(range(epochs)):
+        # Run training and validation steps
         train_loss, train_acc = unet_train_step(model, train_dataloader, loss_fn, accuracy, optimizer, scheduler, weight_fn, device)
         val_loss, val_acc = unet_val_step(model, val_dataloader, loss_fn, accuracy, weight_fn, device)
 
+        # Update the current stats of the epoch
         results["train_loss"].append(train_loss)
         results["train_acc"].append(train_acc)
         results["val_loss"].append(val_loss)
@@ -381,6 +396,7 @@ def unet_train_model(model, train_dataloader, val_dataloader, loss_fn, accuracy,
             # Track the PyTorch model architecture
             # writer.add_graph(model=model, input_to_model=torch.randn(1, 3, 320, 320).to(device)) # Pass in an example input
 
+        # Early stopping
         if early_stopper.early_stop(val_loss):
             print('Model has not improved in 15 epochs.') 
             print('Early stopping.........')            
@@ -389,6 +405,7 @@ def unet_train_model(model, train_dataloader, val_dataloader, loss_fn, accuracy,
     if writer:
         writer.close()
 
+    # Save the best model
     best_model = deepcopy(model.state_dict())
 
     return best_model, results
