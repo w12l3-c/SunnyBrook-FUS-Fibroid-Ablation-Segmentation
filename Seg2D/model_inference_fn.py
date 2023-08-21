0a4813772699fc6adfe66bcd0a3844e0d4db110b
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-21 11:16:27 -0400

    Comment

--- Seg2D/model_inference_fn.py
+++ Seg2D/model_inference_fn.py
@@ -177,6 +177,19 @@ def fpn_inference(test_dataset, model_path, device, num_classes=2, img_size=(320
     FPN.predict_FPN(model, test_dataset, device, img_size)
     
 def manet_inference(test_dataset, model_path, device, num_classes=2, img_size=(320, 320)):
+    """
+    Perform inference using the Mobile Attention Network (MANet) model on a test dataset.
+
+    Args:
+        test_dataset (Dataset): The test dataset to perform inference on.
+        model_path (str): The path to the trained MANet model.
+        device (torch.device): The device to run inference on.
+        num_classes (int, optional): The number of output classes. Default is 2.
+        img_size (tuple, optional): The size of the input images. Default is (320, 320).
+
+    Returns:
+        None
+    """
     # Set Seed
     torch.manual_seed(42)
     
