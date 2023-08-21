535c29b9eafa14ffc44a1c323269ffa200b3d7dd
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-21 09:13:38 -0400

    save

--- Seg2D/save_load.py
+++ Seg2D/save_load.py
@@ -16,15 +16,32 @@ import torch.nn as nn
 # import tensorflow as tf
 # import tensorflow.keras as keras
 
-# ---------------------- Pytorch ---------------------- #
+# ========================= Pytorch ========================= #
 def save_model_torch(model_state_dict, path):
-    torch.save(model_state_dict, path)
+    """
+    Save the architecture and params of a PyTorch model to a pth file.
+
+    Args:
+        model_state_dict (dict): The state dictionary of the PyTorch model.
+        path (str): The file path where the model state dictionary will be saved.
+    """
+    torch.save(model_state_dict, path)  # Save Model
     
 def load_model_torch(model, path):
-    model.load_state_dict(torch.load(path), strict=False)
+    """
+    Load a PyTorch model with its state dictionary from a pth file.
+
+    Args:
+        model (torch.nn.Module): An instance of a PyTorch model where the state dictionary will be loaded.
+        path (str): The file path from which the model state dictionary will be loaded.
+
+    Returns:
+        torch.nn.Module: The PyTorch model with the loaded state dictionary.
+    """
+    model.load_state_dict(torch.load(path), strict=False)   # Load Model
     return model
 
-# ---------------------- Tensorflow ---------------------- #
+# ======================== Tensorflow ======================== #
 # def save_model_tf(model, path):
 #     model.save(path)
 
