695c6330badc400a667518d3d7bc41c67f2507c9
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-21 11:14:31 -0400

    Comment

--- Seg2D/model_inference_fn.py
+++ Seg2D/model_inference_fn.py
@@ -116,6 +116,19 @@ def unetpp_inference(test_dataset, model_path, device, num_classes=2, img_size=(
     
     
 def deeplabv3plus_inference(test_dataset, model_path, device, num_classes=2, img_size=(320, 320)):
+    """
+    Perform inference using the DeepLabV3+ model on a test dataset.
+
+    Args:
+        test_dataset (Dataset): The test dataset to perform inference on.
+        model_path (str): The path to the trained DeepLabV3+ model.
+        device (torch.device): The device to run inference on.
+        num_classes (int, optional): The number of output classes. Default is 2.
+        img_size (tuple, optional): The size of the input images. Default is (320, 320).
+
+    Returns:
+        None
+    """
     # Set Seed
     torch.manual_seed(42)
     
