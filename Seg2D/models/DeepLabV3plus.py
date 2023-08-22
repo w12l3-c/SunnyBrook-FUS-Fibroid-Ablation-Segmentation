ab61b5a5de8e23c4773510900f7b270291070af6
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-22 09:35:19 -0400

    Comment

--- Seg2D/models/DeepLabV3plus.py
+++ Seg2D/models/DeepLabV3plus.py
@@ -25,6 +25,7 @@ from segmentation_models_pytorch.encoders import get_preprocessing_params
 
 # =================== DeepLabV3+ =================== #
 def auto_DEEPLABV3P(in_channels, num_classes, encoder_name='resnet50', encoder_weights='imagenet'):
+    
     model = smp.DeepLabV3Plus(
         encoder_name=encoder_name,       
         encoder_weights=encoder_weights,    
@@ -35,6 +36,15 @@ def auto_DEEPLABV3P(in_channels, num_classes, encoder_name='resnet50', encoder_w
     return model
 
 def prepare_transform(flip=0.3):
+    """
+    Prepare data augmentation transformations.
+
+    Args:
+        flip (float, optional): Probability of horizontal flip. Default is 0.3.
+
+    Returns:
+        torchvision.transforms.Compose: Data transformation pipeline.
+    """
     params = get_preprocessing_params('resnet50', pretrained='imagenet')
     transform = torchvision.transforms.Compose([
         torchvision.transforms.RandomHorizontalFlip(flip),
@@ -44,6 +54,15 @@ def prepare_transform(flip=0.3):
     return transform
 
 def prepare_loss(option='BCE'):
+    """
+    Prepare the loss function.
+
+    Args:
+        option (str, optional): Loss function option ('BCE', 'CE', 'Dice_Binary', 'Dice_Multi'). Default is 'BCE'.
+
+    Returns:
+        torch.nn.Module: Loss function.
+    """
     if option == 'BCE':
         criterion = nn.BCEWithLogitsLoss()  
     if option == 'CE':
@@ -56,6 +75,17 @@ def prepare_loss(option='BCE'):
     return criterion
 
 def prepare_optimizer(model, lr=1e-3, option='Adam'):
+    """
+    Prepare the optimizer.
+
+    Args:
+        model (torch.nn.Module): Model for optimization.
+        lr (float, optional): Learning rate. Default is 1e-3.
+        option (str, optional): Optimizer option ('Adam', 'AdamW', 'SGD'). Default is 'Adam'.
+
+    Returns:
+        torch.optim.Optimizer: Optimizer.
+    """
     if option == 'Adam':
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     if option == 'AdamW':
@@ -65,49 +95,125 @@ def prepare_optimizer(model, lr=1e-3, option='Adam'):
     return optimizer
 
 def prepare_scheduler(optimizer, factor=0.1, patience=10, min_lr=1e-6, verbose=True):
+    """
+    Prepare the learning rate scheduler.
+
+    Args:
+        optimizer (torch.optim.Optimizer): Optimizer for which to schedule learning rates.
+        factor (float, optional): Factor by which to reduce learning rate. Default is 0.1.
+        patience (int, optional): Number of epochs with no improvement before reducing learning rate. Default is 10.
+        min_lr (float, optional): Minimum learning rate. Default is 1e-6.
+        verbose (bool, optional): If True, print a message when learning rate is reduced. Default is True.
+
+    Returns:
+        torch.optim.lr_scheduler.ReduceLROnPlateau: Learning rate scheduler.
+    """
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=factor, patience=patience, min_lr=min_lr, verbose=verbose)
     return scheduler
 
 
-# ===================== Accuracy ===================== #
+# ====================== Accuracy ====================== #
 def accuracy_iou(pred, target):
-    pred_mask = pred > 0.5
+    """
+    Calculate Intersection over Union (IoU) accuracy between predicted and target binary masks.
+
+    Args:
+        pred (torch.Tensor): Predicted binary mask.
+        target (torch.Tensor): Target binary mask.
+
+    Returns:
+        torch.Tensor: IoU accuracy score.
+    """
+    # Create mask for pred and targets
+    pred_mask = pred > 0.5  
     target_mask = target > 0.5
 
+    # Calculate intersection and union base on the mask AND & OR operation
     intersection = torch.sum(pred_mask * target_mask)
     union = torch.sum(pred_mask + target_mask)
 
+    # Calculate IoU
     iou = intersection / union
     return iou
 
 def accuracy_intersect(pred, target):
+    """
+    Calculate accuracy based on the intersection of predicted and target binary masks.
+
+    Args:
+        pred (torch.Tensor): Predicted binary mask.
+        target (torch.Tensor): Target binary mask.
+
+    Returns:
+        torch.Tensor: Intersection accuracy score.
+    """
+    # Create mask for pred and targets  
     pred_mask = pred > 0.5
     target_mask = target > 0.5
+    
+    # Using the AND operation to calculate the intersection
     intersection = torch.sum(pred_mask * target_mask)
     pred_total = torch.sum(pred_mask == True)
     
     return intersection / pred_total
 
 def accuracy_basic(pred, target):
+    """
+    Calculate basic accuracy between predicted and target binary masks.
+
+    Args:
+        pred (torch.Tensor): Predicted binary mask.
+        target (torch.Tensor): Target binary mask.
+
+    Returns:
+        torch.Tensor: Basic accuracy score.
+    """
+    # Create mask for pred and targets
     pred = pred > 0.5
     target = target > 0.5
+    # Simply sum up the number of correct pixels and divide by the total number of pixels
     correct = torch.sum(pred == target)
     return correct / pred.numel()
 
 def accuracy_dice(pred, target):
+    """
+    Calculate Dice coefficient accuracy between predicted and target binary masks.
+
+    Args:
+        pred (torch.Tensor): Predicted binary mask.
+        target (torch.Tensor): Target binary mask.
+
+    Returns:
+        torch.Tensor: Dice coefficient accuracy score.
+    """
+    # Create mask for pred and targets
     pred_mask = pred > 0.5
     target_mask = target > 0.5
 
+    # Calculate intersection and union base on the mask AND & OR operation
     intersection = torch.sum(pred_mask * target_mask)
     total = torch.sum(pred_mask) + torch.sum(target_mask)
 
+    # Dice coefficient formula
     dice = 2 * intersection / total
     return dice
 
 def accuracy_iou_multi(pred, target):
-    pred = torch.argmax(pred, dim=1)
+    """
+    Calculate Intersection over Union (IoU) accuracy for multiple classes in predicted and target masks.
+
+    Args:
+        pred (torch.Tensor): Predicted multi-class mask.
+        target (torch.Tensor): Target multi-class mask.
+
+    Returns:
+        torch.Tensor: Mean IoU accuracy score across classes.
+    """
+    pred = torch.argmax(pred, dim=1)    
     ious = []
+    # Loop through all classes
     for i in torch.unique(target):
+        # Same operation as th regular IOU
         pred_mask = pred == i
         target_mask = target == i
 
@@ -117,51 +223,99 @@ def accuracy_iou_multi(pred, target):
         iou = intersection / union
         ious.append(iou)
     
+    # Return the mean of all classes' IOU
     return torch.mean(torch.tensor(ious))
 
 
-# ===================== Class Weights ===================== #
+# ====================== Class Weights ======================--- #
 def calculate_weights(mask):
-  total = mask.numel()
-  pos = torch.sum(mask > 0.5)
-  return total/pos
+    """
+    Calculate class weights based on the provided mask.
 
+    Args:
+        mask (torch.Tensor): Binary mask.
+
+    Returns:
+        torch.Tensor: Class weights.
+    """ 
+    total = mask.numel()
+    pos = torch.sum(mask > 0.5)
+    return total/pos
 
 # ====================== Gamma Correction ====================== #
 def gamma_correction_cv2(image, gamma=1.0):
+    """
+    Apply gamma correction to an image using OpenCV.
+
+    Args:
+        image (numpy.ndarray): Input image.
+        gamma (float): Gamma correction factor (default is 1.0).
+
+    Returns:
+        numpy.ndarray: Gamma-corrected image.
+    """
     inv_gamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in np.arange(0, 256)])
     return cv2.LUT(image, table.astype(np.uint8))
 
 def gamma_correction_pil(image, gamma=1.0):
+    """
+    Apply gamma correction to an image using PIL (Python Imaging Library).
+
+    Args:
+        image (PIL.Image.Image): Input image.
+        gamma (float): Gamma correction factor (default is 1.0).
+
+    Returns:
+        PIL.Image.Image: Gamma-corrected image.
+    """
     enhancer = ImageEnhance.Brightness(image)
     gamma_corrected_image = enhancer.enhance(gamma)
     return gamma_corrected_image
 
 # ====================== Inference ====================== #
 def predict(model, dataset, device, img_size):
+    """
+    Generate predictions using a PyTorch model on a dataset.
+
+    Args:
+        model (torch.nn.Module): PyTorch model for segmentation.
+        dataset (iterable): Iterable containing image-mask pairs.
+        device (torch.device): Device to run inference on.
+        img_size (tuple): Size to resize input images.
+
+    Yields:
+        tuple: Tuple containing (image, ground truth mask, predicted mask, accuracy scores, inference time).
+    """
+    # Transformation
     transform = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224,0.225])
     ])
     
+    # Set model to evaluation mode
     model.eval()
+    # Inference mode
     with torch.no_grad():
         for pair in dataset:
+            # Get image and mask
             image = pair['img']
             mask = pair['mask']
             
+            # Preprocessing
             image = image.convert('RGB')
             resized_image = image.resize(img_size)
             resized_image = gamma_correction_pil(resized_image, gamma=1.5)
             #resized_image = ImageOps.equalize(resized_image)
             transformed_image = transform(resized_image).to(device)
             
+            # Inference
             start_time = time.time()
             logits = model(transformed_image.unsqueeze(0))
             pred = torch.softmax(logits, dim=1).argmax(dim=1).float()
             end_time = time.time()
             
+            # Calculate accuracy and metrics
             totensor = torchvision.transforms.ToTensor()
             acc_iou = accuracy_iou(pred, totensor(mask.convert('L').resize(img_size)).to(device))
             acc_basic = accuracy_basic(pred, totensor(mask.convert('L').resize(img_size)).to(device))
@@ -170,19 +324,38 @@ def predict(model, dataset, device, img_size):
             pred = pred.squeeze().cpu().numpy() * 255
             pred = cv2.resize(pred, img_size)
             
+            # Resize mask
             if mask is not None:
                 mask = mask.resize(img_size)
             
+            # Calculate inference time
             inference_time = end_time - start_time
             
             yield (image, mask, pred, (acc_iou, acc_basic, acc_dice), inference_time)
             
 def predict_DEEPLABV3P(model, dataset, device, img_size=(320, 320), display=True):
+    """
+    Perform predictions using a UNet model on a dataset and optionally display the results.
+    If `display` is `True`, the user will be prompted to save the predictions of individual inference.
+    If `display` is `False`, it will save a figure where it find the overall metrics for the entire dataset.
+
+    Args:
+        model (torch.nn.Module): UNet model for segmentation.
+        dataset (iterable): Iterable containing image-mask pairs.
+        device (torch.device): Device to run inference on.
+        img_size (tuple): Size to resize input images.
+        display (bool): Whether to display results interactively (default is True).
+    """
+    # Create generator for predictions
     generator = predict(model, dataset, device, img_size)
+    
+    # If display is True, prompt user to save predictions
     if display:
+        # Prompt user to save predictions
         save = input('Save predictions? (y/n): ')
         directory = '/mnt/HDD_1TB/Wallace/Code/Seg2D/predictions/'
-    
+
+        # Create directory if it doesn't exist
         if save == 'y':
             if os.path.exists(directory):
                 print('Directory exists -- Continue')
@@ -190,15 +363,20 @@ def predict_DEEPLABV3P(model, dataset, device, img_size=(320, 320), display=True
                 os.makedirs(directory)
                 print('Directory created')
 
+        # Iterate over generator
         for i, prediction in enumerate(generator):
+            # Unpack prediction
             image, mask, pred, acc, time = prediction
             acc_iou, acc_basic, acc_dice = acc
             # image = gamma_correction_pil(image, gamma=1.5)  
             
+            # If the mask is avaliable do mask operations
             if mask is not None:
+                # Grab the contour of the mask and prediction
                 mask_edge = cv2.Canny(np.asarray(mask), 100, 200)
                 pred_edge = cv2.Canny(pred.astype(np.uint8), 100, 200)
                 
+                # Recolor them into red and green
                 mask_edge_red = np.zeros((mask_edge.shape[0], mask_edge.shape[1], 3))
                 mask_edge_red[mask_edge > 0] = [255, 0, 0]
                 mask_edge_red = mask_edge_red.astype(np.uint8)
@@ -206,9 +384,11 @@ def predict_DEEPLABV3P(model, dataset, device, img_size=(320, 320), display=True
                 pred_edge_green[pred_edge > 0] = [0, 255, 0]
                 pred_edge_green = pred_edge_green.astype(np.uint8)
                 
+                # Overlay them such that the overlapping becomes yellow
                 edge_overlay = mask_edge_red + pred_edge_green
                 edge_overlay = edge_overlay.astype(np.uint8)
                 
+                # Recolouring the prediction and mask in green and red respectively
                 mask_red = np.zeros((mask.size[1], mask.size[0], 3))
                 pred_green = np.zeros((mask.size[1], mask.size[0], 3))
                 
@@ -221,9 +401,11 @@ def predict_DEEPLABV3P(model, dataset, device, img_size=(320, 320), display=True
                 mask_red = mask_red.astype(np.uint8)
                 pred_green = pred_green.astype(np.uint8)
                 
+                # Overlay them so make the overlapping yellow
                 mask_overlay = mask_red + pred_green
                 mask_overlay = mask_overlay.astype(np.uint8)
             
+            # Grid plot of image, mask, and prediction
             fig, ax = plt.subplots(2,3, figsize=(20,15))
             
             ax[0][0].imshow(image)
@@ -254,23 +436,29 @@ def predict_DEEPLABV3P(model, dataset, device, img_size=(320, 320), display=True
             fig.suptitle(f'Inference Time: {time:.4f} seconds')
             plt.show()
             
+            # Save the prediction if user wants to
             if save == 'y':
                 filename = f"predictions/mask_{i}.jpg"
                 # cv2.imwrite(os.path.join(directory, filename), pred)
                 fig.savefig(filename)
-                
+            
+            # Prompt user to exit every 10 images
             if i % 10 == 0:
                 quit = input('Exit? (y/n): ')
                 if quit == 'y':
                     break
             
         print('Inference Complete')
-        
+    
+    # If display is False, return accuracy metrics
     else:
+        # Save accuracy metrics
         acc_ious = []
         acc_basics = []
         acc_dices = []
         times = []
+        
+        # Iterate over generator
         for prediction in generator:
             image, mask, pred, acc, time = prediction
             acc_iou, acc_basic, acc_dice = acc
@@ -278,7 +466,8 @@ def predict_DEEPLABV3P(model, dataset, device, img_size=(320, 320), display=True
             acc_basics.append(acc_basic.item())
             acc_dices.append(acc_dice.item())
             times.append(time)
-            
+        
+        # Plot accuracy metrics
         fig, ax = plt.subplots(2, 2, figsize=(20,10))
         
         ax[0][0].set_title(f'Accuracy (IOU) | Median:{np.median(np.array(acc_ious))*100:.4f}')
@@ -292,6 +481,7 @@ def predict_DEEPLABV3P(model, dataset, device, img_size=(320, 320), display=True
         
         plt.show()
         
-        save_path = f'metrics_{uuid.uuid1}.png'
+        # Save the metrics
+        save_path = f'predictions/metrics_{uuid.uuid1}.png' # uuid is a unique string generator
         fig.savefig(save_path)
         print(f'Metrics saved to {save_path}')
