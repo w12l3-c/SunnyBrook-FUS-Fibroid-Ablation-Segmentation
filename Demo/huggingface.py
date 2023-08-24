df0468d1b948914db83159657dd6606ae3a3323b
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-24 09:04:55 -0400

    Delete File

--- Demo/huggingface.py
+++
@@ -1,8 +0,0 @@
-import huggingface_hub
-
-
-# ------------- HuggingFace Repo Git ------------- #
-huggingface_hub.create_repo()
-huggingface_hub.add_file("README.md")
-huggingface_hub.commit("Initial commit")
-huggingface_hub.push_to_hub()
