426bf90d05e9d38bf550dbf6eb79da03028e6432
wallace <97126484+w12l3-c@users.noreply.github.com>
2023-08-24 09:12:53 -0400

    Add Loading Model

--- Demo/SpineSeg/model.py
+++ Demo/SpineSeg/model.py
@@ -6,7 +6,7 @@ from torch import nn
 import segmentation_models_pytorch as smp
 from segmentation_models_pytorch.encoders import get_preprocessing_params
 
-def create_Unet(in_channels=3, num_classes=1):
+def create_Unet(in_channels=3, num_classes=2):
     model = smp.Unet(
             encoder_name="resnet101",       
             encoder_weights="imagenet",    
@@ -21,3 +21,17 @@ def create_Unet(in_channels=3, num_classes=1):
     ])
         
     return model, transform
+
+def load_model_torch(model, path):
+    """
+    Load a PyTorch model with its state dictionary from a pth file.
+
+    Args:
+        model (torch.nn.Module): An instance of a PyTorch model where the state dictionary will be loaded.
+        path (str): The file path from which the model state dictionary will be loaded.
+
+    Returns:
+        torch.nn.Module: The PyTorch model with the loaded state dictionary.
+    """
+    model.load_state_dict(torch.load(path), strict=False)   # Load Model
+    return model
